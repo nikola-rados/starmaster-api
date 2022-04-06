d772c3ef3df7c7ac2395ed71233a2d6149568c05
Nik Radoš <nrados@uvic.ca>
2022-04-05 21:42:42 -0700

    Add ability score class

---
+++ app/models/ability_scores.py
@@ -0,0 +1,7 @@
+class AbilityScores:
+    def __init__(self, strength: int, dexterity: int, constitution: int, intelligence: int, wisdom: int):
+        self.strength = strength
+        self.dexterity = dexterity
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
